49d16f127c0c0ebe32ecc94b2edeeb7a4858bcd3
李茂益 <limaoyi@tianhecloud.com>
2023-08-10 15:01:16 +0800

    新增到影评人

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="gpt-prompt",
-    version="0.0.3",
+    version="0.0.4",
     author="limaoyi",
     author_email="limaoyi@qq.com",
     description="Convenient for Python users to directly reference the source code of the GPT question template "
